b89d48b29629439b5ad94b068dc47d090a0b4ef0
Denisson Pereira Santos <denisson159@gmail.com>
2023-10-18 18:42:59 -0300

    element 7

--- elements.py
+++ elements.py
@@ -3,10 +3,62 @@
 elementos = {
     "ola": ["Olá! Digite o nome do elemento: ", "Olá!", "Oi"],
     "1": [
-        "\nNome: Hidrogênio; \nNúmero atômico: 1; \nSímbolo: H; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1", "\nNome: Hidrogênio; \nNúmero atômico: 1; \nSímbolo: H; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1s1"
+        "\nNome: Hidrogênio; \nSímbolo: H; \nNúmero atômico: 1; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1s¹", 
+        "\nNome: Hidrogênio; \nSímbolo: H;  \nZ: 1; \nA: 1,0079; \nDistribuição eletrônica: 1"
     ],
     "hidrogenio": [
-        "\nNome: Hidrogênio; \nNúmero atômico: 1; \nSímbolo: H; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1", "\nNome: Hidrogênio; \nNúmero atômico: 1; \nSímbolo: H; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1s1"
-    ]
+        "\nNome: Hidrogênio; \nSímbolo: H; \nNúmero atômico: 1; \nMassa atômica: 1,0079; \nDistribuição eletrônica: 1s¹", 
+        "\nNome: Hidrogênio; \nSímbolo: H;  \nZ: 1; \nA: 1,0079; \nDistribuição eletrônica: 1" 
+    ],
+    "2": [
+        "\nNome: Hélio; \nSímbolo: He; \nNúmero atômico: 2; \nMassa atômica: 4,0026; \nDistribuição eletrônica: 1s²", 
+        "\nNome: Hélio; \nSímbolo: He;  \nZ: 2; \nA: 4,0026; \nDistribuição eletrônica: 1"
+    ],
+    "helio": [
+        "\nNome: Hélio; \nSímbolo: He; \nNúmero atômico: 2; \nMassa atômica: 4,0026; \nDistribuição eletrônica: 1s²", 
+        "\nNome: Hélio; \nSímbolo: He;  \nZ: 2; \nA: 4,0026; \nDistribuição eletrônica: 1"
+    ],
+    "3": [
+        "\nNome: Lítio; \nSímbolo: Li; \nNúmero atômico: 3; \nMassa atômica: 6,941; \nDistribuição eletrônica: 1s² 2s¹", 
+        "\nNome: Lítio; \nSímbolo: Li;  \nZ: 3; \nA: 6,941; \nDistribuição eletrônica: 1"
+    ],
+    "litio": [
+        "\nNome: Lítio; \nSímbolo: Li; \nNúmero atômico: 3; \nMassa atômica: 6,941; \nDistribuição eletrônica: 1s² 2s¹", 
+        "\nNome: Lítio; \nSímbolo: Li;  \nZ: 3; \nA: 6,941; \nDistribuição eletrônica: 1"
+    ],
+        "4": [
+        "\nNome: Berílio; \nSímbolo: Be; \nNúmero atômico: 4; \nMassa atômica: 9,0122; \nDistribuição eletrônica: 1s² 2s²", 
+        "\nNome: Berílio; \nSímbolo: Be;  \nZ: 4; \nA: 9,0122; \nDistribuição eletrônica: 1"
+    ],
+    "berilio": [
+        "\nNome: Berílio; \nSímbolo: Be; \nNúmero atômico: 4; \nMassa atômica: 9,0122; \nDistribuição eletrônica: 1s² 2s²", 
+        "\nNome: Berílio; \nSímbolo: Be;  \nZ: 4; \nA: 9,0122; \nDistribuição eletrônica: 1"
+    ],
+    "5": [
+        "\nNome: Boro; \nSímbolo: B; \nNúmero atômico: 5; \nMassa atômica: 10,81; \nDistribuição eletrônica: 1s² 2s² 2p¹", 
+        "\nNome: Boro; \nSímbolo: B;  \nZ: 5; \nA: 10,81; \nDistribuição eletrônica: 1"
+    ],
+    "boro": [
+        "\nNome: Boro; \nSímbolo: B; \nNúmero atômico: 5; \nMassa atômica: 10,81; \nDistribuição eletrônica: 1s² 2s² 2p¹", 
+        "\nNome: Boro; \nSímbolo: B;  \nZ: 5; \nA: 10,81; \nDistribuição eletrônica: 1"
+    ],
+    "6": [
+        "\nNome: Carbono; \nSímbolo: C; \nNúmero atômico: 6; \nMassa atômica: 12,01; \nDistribuição eletrônica: 1s² 2s² 2p²", 
+        "\nNome: Carbono; \nSímbolo: C;  \nZ: 6; \nA: 12,01; \nDistribuição eletrônica: 1"
+    ],
+    "carbono": [
+        "\nNome: Carbono; \nSímbolo: C; \nNúmero atômico: 6; \nMassa atômica: 12,01; \nDistribuição eletrônica: 1s² 2s² 2p²", 
+        "\nNome: Carbono; \nSímbolo: C;  \nZ: 6; \nA: 12,01; \nDistribuição eletrônica: 1"
+    ],
+    "7": [
+        "\nNome: Nitrogênio; \nSímbolo: N; \nNúmero atômico: 7; \nMassa atômica: 14,01; \nDistribuição eletrônica: 1s² 2s² 2p³", 
+        "\nNome: Nitrogênio; \nSímbolo: N;  \nZ: 7; \nA: 14,01; \nDistribuição eletrônica: 1"
+    ],
+    "nitrogenio": [
+        "\nNome: Nitrogênio; \nSímbolo: N; \nNúmero atômico: 7; \nMassa atômica: 14,01; \nDistribuição eletrônica: 1s² 2s² 2p³", 
+        "\nNome: Nitrogênio; \nSímbolo: N;  \nZ: 7; \nA: 14,01; \nDistribuição eletrônica: 1"
+    ],
+    
+
 
 }
